6dbc975fb26be427827d12046519a5f872e41682
ffreylaw <ffreylaw@gmail.com>
2017-05-21 17:44:42 +1000

    update

--- amazing_escape/core/src/mycontroller/ThreePointTurn.java
+++ amazing_escape/core/src/mycontroller/ThreePointTurn.java
@@ -1,5 +1,9 @@
 package mycontroller;
 
+import java.util.HashMap;
+
+import tiles.MapTile;
+import utilities.Coordinate;
 import world.WorldSpatial;
 
 public class ThreePointTurn implements DeadEndAction {
@@ -7,12 +11,26 @@ public class ThreePointTurn implements DeadEndAction {
 	private int point;
 	
 	public ThreePointTurn() {
-		this.point = 0;
+		this.point = 1;
 	}
 
 	@Override
 	public void action(MyAIController controller, float delta) {
-		
+		switch (point) {
+		case 1:
+			applyFirstPoint(controller, controller.getOrientation(), delta);
+			break;
+		case 2:
+			applySecondPoint(controller, controller.getOrientation(), delta);
+			break;
+		case 3:
+			applyThirdPoint(controller, controller.getOrientation(), delta);
+			break;
+		}
+	}
+	
+	private boolean checkEndPoint(Coordinate currentPosition, HashMap<Coordinate, MapTile> currentView) {
+		return false;
 	}
 	
 	private void applyFirstPoint(MyAIController controller, WorldSpatial.Direction orientation, float delta) {
